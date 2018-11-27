674313a66c8168f160ea419a7f38a2795257d1a7
MathieuEtchepare <mat.etche91@hotmail.fr>
2018-11-27 16:10:11 +0100

    affichage

---
+++ game.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "initplate.h"
+#define clear() printf("\033[H\033[J")
+
+void printGame(char b1[10][10], char b2[10][10]){
+   int i,j;
+   clear();
+   printf("\n Main Board \t\t\tMark Board\n");
+   for(i = 0; i < 10; i++){
+        if(i!=9)printf(" %d  ", i+1);
+	else printf(" %d ", i+1);
+	for(j = 0; j < 10; j++)printf("%c ", b1[i][j]);
+	printf("	");
+	for(j = 0; j < 10; j++)printf("%c ", b2[i][j]);
+        printf("\n");
+   }
+   printf("    A B C D E F G H I J         A B C D E F G H I J\n\n");
+
+}
+
+int main(){
+
+   
+   char MyBoard[10][10];
+   char MarkBoard[10][10];
+   
+   initplate(MyBoard);
+   initplate(MarkBoard);
+   printGame(MyBoard, MarkBoard);
+   
+   return 0;
+}
