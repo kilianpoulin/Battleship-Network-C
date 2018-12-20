d8d399360b5e05304cb443772ed851352440e498
kilianpoulin <kilian.poulin@efrei.net>
2018-12-20 20:46:27 +0100

    game initialization - placing ships
    Placing ships manually or randomly

--- game.c
+++ game.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "initplate.h"
+#include "game.h"
+#include <stdlib.h>
 #define clear() printf("\033[H\033[J")
 
 void printGame(char b1[10][10], char b2[10][10]){
    int i,j;
-   clear();
+//   clear();
    printf("\n Main Board \t\t\tMark Board\n");
    for(i = 0; i < 10; i++){
         if(i!=9)printf(" %d  ", i+1);
@@ -18,15 +20,39 @@ void printGame(char b1[10][10], char b2[10][10]){
 
 }
 
-int main(){
+void printBoard(char b1[10][10]){
+   int i,j;
+   printf("\n Main Board \t\t\tMark Board\n");
+   for(i = 0; i < 10; i++){
+        if(i!=9)printf(" %d  ", i+1);
+	else printf(" %d ", i+1);
+	for(j = 0; j < 10; j++)printf("%c ", b1[i][j]);
+	printf("\n");
+   }
+   printf("    A B C D E F G H I J\n\n");
 
-   
-   char MyBoard[10][10];
-   char MarkBoard[10][10];
-   
+}
+
+int main(){
+    srand(time(NULL));
+    char MyBoard[10][10];
+    char MarkBoard[10][10];
+    int choice;
+
    initplate(MyBoard);
    initplate(MarkBoard);
    printGame(MyBoard, MarkBoard);
-   
+
+   printf("Init board\n");
+   printf("[0] Manually | [1] Randomly ==> ");
+    scanf("%d", &choice);
+    if(choice == 0)
+        initships(MyBoard, 0);
+    else
+        initships(MyBoard, 1);
+
+   printGame(MyBoard, MarkBoard);
+
+
    return 0;
 }
