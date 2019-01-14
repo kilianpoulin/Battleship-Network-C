a604f7124f8e3b45c670f8f8b249f6ed1f1e0217
kilianpoulin <kilian.poulin@efrei.net>
2019-01-14 21:59:38 +0100

    2 bugs fixed, 1 improvement
    Improvement :
    A message is displayed to tell the player if his move hit anything or not
    
    Bugs fixed  :
    - When there is no more upper case letters on board => we have a winner
    
    - A player cannot hit the same location twice / if location already hit he has to start again his selection

--- game.c
+++ game.c
@@ -20,7 +20,6 @@ void printGame(char b1[10][10], char b2[10][10]){
 
 }
 
-
 void printBoard(char b1[10][10]){
    int i,j;
    printf("\n Main Board \t\t\tMark Board\n");
@@ -42,24 +41,39 @@ void fire_missile(Action a, char board[10][10], char mark[10][10]){
     if(board[a.x][a.y] != '-' && board[a.x][a.y] > 64 && board[a.x][a.y] < 91){
         board[a.x][a.y] = board[a.x][a.y] + 32;
         mark[a.x][a.y] = 'X'; /// hit
+        printf(" RESULT : Congratulations, it's a hit !\n\n");
     }
     else {
         mark[a.x][a.y] = 'x'; /// missed
+        printf(" RESULT : Sorry, you missed !\n\n");
     }
 }
 
 /// METHOD : game on
 /// checks if player's board has any upper case letters
-/// if not, he is the winner
+/// if not, he is the loser
+/// returns 0 if loser
 int game_on(char p[10][10]){
     int i, j;
     for(i = 0; i < 10; i++){
         for(j = 0; j < 10; j++){
-            if(p[i][j] != '-')
+            if(p[i][j] > 64 && p[i][j] < 92)
                 return 1;
         }
     }
+    return 0;
+}
 
+/// METHOD : check_hit_exists
+/// checks if the player already fired a missile on this place
+/// by checking his mark board
+/// returns 0 if doesn't exist
+/// returns 1 if exists
+int check_hit_exists(Action action, char mark[10][10]){
+    if(mark[action.x][action.y] != '-'){
+        printf(" WARNING : You have already fired a missile on this spot, please enter another one. \n\n");
+        return 1;
+    }
     return 0;
 }
 
@@ -71,40 +85,43 @@ void play_game(char board[10][10], char Cpboard[10][10], char mark[10][10], char
     int winner = 0;
     do{
         /// HUMAN PLAYER
-        printf("\n \t Player 1 : It's your turn to play !\n");
-        printf("Case where to drop a missile (ex : A1) : ");
-        scanf("%s", &choice);
+        printf("\n \t ----- Player 1 : It's your turn to play ! ----- \n\n");
+        do{
+            printf("Case where to drop a missile (ex : A1) : ");
+            scanf("%s", &choice);
 
-        /// Setting Y
-        action.y = convert_vertical_coor(choice[0]); /// converts letter coordinate into number
+            /// Setting Y
+            action.y = convert_vertical_coor(choice[0]); /// converts letter coordinate into number
 
-        /// Setting X
-        if(choice[2] != '\0')
-            action.x = 9;
-        else
-            action.x = convert_horizontal_coor(choice[1]); /// converts char 'number' coordinate into number
+            /// Setting X
+            if(choice[2] != '\0')
+                action.x = 9;
+            else
+                action.x = convert_horizontal_coor(choice[1]); /// converts char 'number' coordinate into number
+        /// check if player already hit that place
+        } while(check_hit_exists(action, mark) != 0);
 
         fire_missile(action, Cpboard, mark);
-
-        printf("--- Human player ---\n");
         printGame(board, mark);
         printf("\n\n");
 
         /// COMPUTER
-        action.x = rand()%10;
-        action.y = rand()%10;
-        fire_missile(action, board, Cpmark);
-
-        printf("--- Computer ---\n");
-        printGame(Cpboard, Cpmark);
+        printf("\t ----- Computer's turn to play ! ----- \n\n");
+        do{
+            action.x = rand()%10;
+            action.y = rand()%10;
+            /// check if player already hit that place
+        } while(check_hit_exists(action, Cpmark) != 0);
+            fire_missile(action, board, Cpmark);
+        //printGame(Cpboard, Cpmark);
         printf("\n\n");
 
         winner = 0;
         /// Check if there is a winner
         if(game_on(board) == 0)
-            printf("Player 1 wins !!!! ");
+            printf("Computer wins !!!! ");
         else if(game_on(Cpboard) == 0)
-            printf("Computer wins !!!");
+            printf("Player 1  wins !!!");
         else
             winner = 1;
 
@@ -136,7 +153,7 @@ int main(){
     /// COMPUTER
     initplate(CpBoard);
     initplate(CpMarkBoard);
-    printGame(CpBoard, CpMarkBoard);
+    //printGame(CpBoard, CpMarkBoard);
     initships(CpBoard, 1); /// generated randomly
 
 
